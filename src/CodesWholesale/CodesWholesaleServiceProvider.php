e6281b287bfa57bd53ffbfc2961a8efa105add4c
Mircea Pop/squirreldev <mirceaflorinpop@gmail.com>
2015-07-12 17:37:40 +0300

    fixed bug causing syntax error

--- src/CodesWholesale/CodesWholesaleServiceProvider.php
+++ src/CodesWholesale/CodesWholesaleServiceProvider.php
@@ -1,28 +1,17 @@
 <?php namespace CodesWholesale;
 
 use Illuminate\Support\ServiceProvider;
-use CodesWholesale\Config;
-use CodesWholesale\ClientBuilder;
 
 class CodesWholesaleServiceProvider extends ServiceProvider {
-
-    // /**
-    //  * Bootstrap the application events.
-    //  *
-    //  * @return void
-    //  */
-    // public function boot() {
-
-    // }
-
     /**
      * Register the service provider.
      *
      * @return void
      */
     public function register() {
-      $this->app->singleton('CodesWholesale\ClientBuilder', function($app) {
-        return new \CodesWholesale\ClientBuilder(\CodesWholesale\Config::getConfig())->build();
+      $this->app->singleton('CodesWholesale', function($app) {
+        $instance = CodesWholesale\ClientBuilder(CodesWholesale\Config::getConfig())->build();
+        return $instance;
       });
     }
 }
